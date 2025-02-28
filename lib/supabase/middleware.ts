83c77a2594fbef7c9129fa0fe81b6066bbb6d74a
CharlieAlbert <charlieraph36@gmail.com>
2025-02-28 21:28:19 +0300

    touch base!

---
+++ lib/supabase/middleware.ts
@@ -0,0 +1,76 @@
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { type NextRequest, NextResponse } from "next/server";
+
+export const updateSession = async (request: NextRequest) => {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+        remove(name: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+        },
+      },
+    }
+  );
+
+  await supabase.auth.getUser();
+
+  response.headers.set("x-url", request.url);
+
+  return response;
+};
+
+export const forceLoginWithRedirect = async (request: NextRequest) => {
+  const originalUrl = new URL(request.url);
+  const path = originalUrl.pathname;
+  const query = originalUrl.searchParams.toString();
+  return NextResponse.redirect(
+    new URL(
+      `/login?redirect=${encodeURIComponent(
+        path + (query ? `?${query}` : "")
+      )}`,
+      request.url
+    )
+  );
+};
